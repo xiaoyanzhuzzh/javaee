4834df952cd63dc18510cbf5a728e5a9bc1cabb2
xiaoyanzhuzzh <xiaoyanzhuzzh@sina.com>
2015-07-08 15:40:30 +0800

    create userDao class file and user class

---
+++ core/src/main/java/com/tw/core/dao/userDao.java
@@ -0,0 +1,7 @@
+package com.tw.core.dao;
+
+/**
+ * Created by zhihuizhang on 7/8/15.
+ */
+public class userDao {
+}
---
+++ core/src/main/java/com/tw/core/entity/user.java
@@ -0,0 +1,7 @@
+package com.tw.core.entity;
+
+/**
+ * Created by zhihuizhang on 7/8/15.
+ */
+public class user {
+}
