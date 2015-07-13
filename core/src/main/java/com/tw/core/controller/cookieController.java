f8ffea82ed9250031be7f3520ef118d8c4c06a62
xiaoyanzhuzzh <xiaoyanzhuzzh@sina.com>
2015-07-13 17:32:28 +0800

    add core controller java code

--- core/src/main/java/com/tw/core/controller/cookieController.java
+++ core/src/main/java/com/tw/core/controller/cookieController.java
@@ -1,7 +1,29 @@
 package com.tw.core.controller;
 
-/**
- * Created by zhihuizhang on 7/13/15.
- */
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@RestController
+@RequestMapping("/")
 public class cookieController {
+    @RequestMapping(method = RequestMethod.GET)
+    public ModelAndView getLoginPage(){
+
+        ModelAndView modelAndView = new ModelAndView();
+
+        modelAndView.setViewName("login");
+        return modelAndView;
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public ModelAndView login(){
+
+        ModelAndView modelAndView = new ModelAndView();
+
+        modelAndView.setViewName("login");
+        return modelAndView;
+    }
+
 }
