b877a25064c074bbe557d9e620600a6d71cdb4ee
xiaoyanzhuzzh <xiaoyanzhuzzh@sina.com>
2015-07-14 15:47:43 +0800

    add Dao class of admin

--- core/src/main/java/com/tw/core/dao/AdminDao.java
+++ core/src/main/java/com/tw/core/dao/AdminDao.java
@@ -1,7 +1,32 @@
 package com.tw.core.dao;
 
-/**
- * Created by zhihuizhang on 7/14/15.
- */
+import com.tw.core.entity.Admin;
+import com.tw.core.util.HibernateUtil;
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
 public class AdminDao {
+
+    public Boolean verifyAdminInfo(String name, String password) {
+        Boolean result = false;
+        Session session = HibernateUtil.getSessionFactory().openSession();
+
+        String hql = "from Admin where name=? and password=?";
+        Query query = session.createQuery(hql);
+
+        query.setString(0, name);
+        query.setString(1, password);
+
+        List<Admin> admins = query.list();
+        if(admins.size() == 1) {
+            result = true;
+        }
+        session.close();
+
+        return result;
+    }
 }
