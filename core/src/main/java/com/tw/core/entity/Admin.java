5b415268d2dd476e46f29fc8e443ef951b38bad4
xiaoyanzhuzzh <xiaoyanzhuzzh@sina.com>
2015-07-14 15:47:10 +0800

    complete entity Admin class

--- core/src/main/java/com/tw/core/entity/Admin.java
+++ core/src/main/java/com/tw/core/entity/Admin.java
@@ -1,7 +1,45 @@
 package com.tw.core.entity;
 
-/**
- * Created by zhihuizhang on 7/14/15.
- */
+import javax.persistence.*;
+
+@Entity
+@Table(name = "admin")
 public class Admin {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "password")
+    private String password;
+
+    public Admin() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
