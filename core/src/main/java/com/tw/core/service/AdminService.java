51efa55b3b846bbf923300000637be763a7534a2
xiaoyanzhuzzh <xiaoyanzhuzzh@sina.com>
2015-07-14 16:28:50 +0800

    delete useless codes

--- core/src/main/java/com/tw/core/service/AdminService.java
+++ core/src/main/java/com/tw/core/service/AdminService.java
@@ -2,19 +2,16 @@ package com.tw.core.service;
 
 import com.tw.core.dao.AdminDao;
 import com.tw.core.entity.Admin;
-import com.tw.core.util.HibernateUtil;
-import org.hibernate.Query;
-import org.hibernate.Session;
+import com.tw.core.helper.EncryptionHelper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 @Service
 public class AdminService {
 
     @Autowired
     private AdminDao adminDao;
+
     public Boolean verifyAdminInfo(String name, String password) {
         return adminDao.verifyAdminInfo(name, password);
     }
