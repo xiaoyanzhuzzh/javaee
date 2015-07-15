811a2e3c0847652674489f2d7c0eb48823fb622e
xiaoyanzhuzzh <xiaoyanzhuzzh@sina.com>
2015-07-15 12:55:50 +0800

    create Url helper class

--- core/src/main/java/com/tw/core/helper/UrlHelper.java
+++ core/src/main/java/com/tw/core/helper/UrlHelper.java
@@ -1,7 +1,34 @@
 package com.tw.core.helper;
 
-/**
- * Created by zhihuizhang on 7/15/15.
- */
-public class UrlHelper {
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class UrlHelper extends HandlerInterceptorAdapter{
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        //1、请求到登录页面 放行
+        String loginUrl = "/login";
+        System.out.println("++++++++++++++");
+        if(request.getServletPath().startsWith(loginUrl)) {
+            return true;
+        }
+
+        //2、TODO 比如退出、首页等页面无需登录，即此处要放行 允许游客的请求
+
+        //3、如果用户已经登录 放行
+        System.out.println(request.getSession().getAttribute("currentUser"));
+        if(request.getSession().getAttribute("currentUser") != null) {
+            //更好的实现方式的使用cookie
+            return true;
+        }
+
+        //4、非法请求 即这些请求需要登录后才能访问
+        //重定向到登录页面
+        System.out.println(request.getContextPath());
+        response.sendRedirect(request.getContextPath() + loginUrl);
+        return false;
+    }
 }
